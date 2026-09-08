97a0a5310c5cbd9a9047f5ef54eda54411115192
Raja Surendra <183709973+rajasurendrag@users.noreply.github.com.>
2026-09-08 09:07:06 +0530

    Removes build artifacts from version control
    build/ and my_rag.egg-info/ are generated by setuptools on install/build and don't need to be tracked.

--- build/lib/app/graph/state.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated, TypedDict
-
-from langchain_core.messages import BaseMessage
-from langgraph.graph.message import add_messages
-
-
-class RAGState(TypedDict):
-  messages: Annotated[list[BaseMessage], add_messages]
-  context: list
